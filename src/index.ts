a9b0bb6cc2158e12bfcad959b87d20b515cce162
ackerman <julio.sergio2709@gmail.com>
2023-06-10 00:33:18 -0400

    🧙‍♂️: sse connection interval

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import bodyParser from "body-parser";
-import express from "express";
+import express, { Request, Response } from "express";
 import pg from "pg";
 import path from "path";
 
@@ -19,10 +19,60 @@ app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 app.use(cors())
 
+//
+
 app.get("/", async (req, res) => {
   const { rows } = await pool.query("SELECT NOW()");
   res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
 });
+
+
+let clientData: { res: Response, clientNo: number, messageNo: number }[] = [];
+let sequence = 1;
+
+app.get('/sse', (req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+  addSSEClient(req, res);
+});
+
+function addSSEClient(req: Request, res: Response) {
+  req.on('close', () => clientData = clientData.filter(o => o.res !== res));
+  clientData.push({
+    res,
+    clientNo: sequence++,
+    messageNo: 1
+  });
+  if (clientData.length === 0 || clientData.length === 1) {
+    console.log("client atached");
+    setTimeout(send);
+  }
+}
+
+function send() {
+  console.log('data sended');
+  clientData.forEach(o => {
+    const data = JSON.stringify({
+      clientCount: clientData.length,
+      clientNo: o.clientNo,
+      messageNo: o.messageNo++
+    });
+    o.res.write(`data: ${data}\n\n`);
+  });
+  setTimeout(() => {
+    if (clientData.length === 0) {
+      console.log("iterations stopped");
+    } else {
+      send()
+    }
+  }, 1000);
+}
+setTimeout(send);
+
+//
 
 app.get("/api/consumer", async (req, res) => {
   const filePath = path.join(__dirname, 'index.html');
