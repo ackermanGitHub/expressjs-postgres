8576ab041b474e44c98369627741a177acb15bc6
ackerman <julio.sergio2709@gmail.com>
2023-06-10 05:10:51 -0400

    🔧: ... 5.0

--- src/index.ts
+++ src/index.ts
@@ -70,115 +70,9 @@ function send() {
     }
   }, 1000);
 }
-setTimeout(send);
 
 //
-
-app.get("/api/consumer", async (req, res) => {
-  const filePath = path.join(__dirname, 'index.html');
-  res.sendFile(filePath);
-});
-
-const httpServer = app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-import WebSocket from "ws";
-
-const wsServer = new WebSocket.Server({ noServer: true })
-
-httpServer.on('upgrade', (req, socket, head) => {
-  console.log("new connection")
-  wsServer.handleUpgrade(req, socket, head, (ws) => {
-    wsServer.emit('connection', ws, req)
-  })
-})
-
-wsServer.on('connection', (ws) => {
-  console.log("new connection");
-  ws.on('message', (message) => {
-    console.log('Received message:', message.toString());
-    ws.send(`You said: ${message}`);
-  });
-});
-
-
 /* 
-import bodyParser from "body-parser";
-import express, { Request, Response } from "express";
-import pg from "pg";
-import path from "path";
-
-import dotenv from "dotenv";
-import cors from 'cors'
-dotenv.config()
-
-// Connect to the database using the DATABASE_URL environment
-//   variable injected by Railway
-const pool = new pg.Pool();
-
-const app = express();
-const port = process.env.PORT || 3333;
-
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
-app.use(cors())
-
-//
-
-app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
-});
-
-
-let clientData: { res: Response, clientNo: number, messageNo: number }[] = [];
-let sequence = 1;
-
-app.get('/sse', (req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  });
-  addSSEClient(req, res);
-});
-
-function addSSEClient(req: Request, res: Response) {
-  req.on('close', () => clientData = clientData.filter(o => o.res !== res));
-  clientData.push({
-    res,
-    clientNo: sequence++,
-    messageNo: 1
-  });
-  if (clientData.length === 0 || clientData.length === 1) {
-    console.log("client atached");
-    setTimeout(send);
-  }
-}
-
-function send() {
-  console.log('data sended');
-  clientData.forEach(o => {
-    const data = JSON.stringify({
-      clientCount: clientData.length,
-      clientNo: o.clientNo,
-      messageNo: o.messageNo++
-    });
-    o.res.write(`data: ${data}\n\n`);
-  });
-  setTimeout(() => {
-    if (clientData.length === 0) {
-      console.log("iterations stopped");
-    } else {
-      send()
-    }
-  }, 1000);
-}
-
-//
-
 
 app.get("/api/consumer", async (req, res) => {
   const filePath = path.join(__dirname, 'index.html');
@@ -192,9 +86,9 @@ app.get("/api/clients", async (req, res) => {
   res.send(`Hello, World! There are ${clients.size} clients connected`);
 });
 
-const httpServer = app.listen(port, () => {
+const httpServer =  */app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-});
+});/* 
 
 import WebSocket from "ws";
 
@@ -250,4 +144,4 @@ wsServer.on('connection', (ws, request) => {
     clients.delete(ws);
   });
 });
-*/
+ */
